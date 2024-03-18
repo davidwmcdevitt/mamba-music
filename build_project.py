f47d8b759a84bde6007cae1bfdc745ac6987d712
davidwmcdevitt <31931152+davidwmcdevitt@users.noreply.github.com>
2024-03-17 20:32:50 -0500

    Create build_project.py

---
+++ build_project.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def parse_args():
+    
+    parser = argparse.ArgumentParser(description='Mamba-Music Project Setup')
+    
+    parser.add_argument('--project_dir', type=str, required=True, help='Path to GitHub repo')
+    
+    return parser.parse_args()
+    
+
+
+def build_project():
+    pass
+
+
+
+if __name__ == "__main__":
+    
+    args = parse_args()
+    
+    build_project(args)
