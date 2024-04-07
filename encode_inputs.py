80487efb9409af9daf031e250e09fe6714defd73
davidwmcdevitt <31931152+davidwmcdevitt@users.noreply.github.com>
2024-04-07 16:35:48 -0500

    Update encode_inputs.py

--- encode_inputs.py
+++ encode_inputs.py
@@ -93,7 +93,9 @@ def analyze_vocab(args):
     
     unique_tuples = [tuple(row) for row in unique_tuples]
     
-    np.save(os.path.join(project_dir,'configs/vocab.npy'), unique_tuples)
+    vocab_size = len(unique_tuples)
+    
+    np.save(os.path.join(project_dir, f'configs/vocab_{vocab_size}.npy'), unique_tuples)
     
     for track in tqdm(tracks):
 
